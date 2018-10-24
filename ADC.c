2145fe4d5021849cae14cc20e3083c6edfd78588
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-10-24 18:18:04 -0500

    ADC missing printing in screen

--- ADC.c
+++ ADC.c
@@ -9,10 +9,14 @@
 #include "ADC.h"
 #include "DataTypeDefinitions.h"
 #include "GPIO.h"
+#include "PWM.h"
 
 uint16 result;
 uint16 adcResult;
 uint16 adcTemporal;
+sint16 Red;
+sint16 Green;
+sint16 Blue;
 /*Port C Pin 10: ADC1_SE6b */
 
 gpio_pin_control_register_t Default_function = GPIO_MUX0;
@@ -40,58 +44,112 @@ void ADC_init (void){
 	 adcTemporal = ADC_result();
 	 	 if((ADC_NULL < adcTemporal) && (adcTemporal < ADC_RED))
 	 	 {
-
+	 		 Red = 255;
+	 		 Green = 0;
+	 		 Blue = 0;
+	 		 PWM_channel_value(Red, Green, Blue);
 	 	 }
 	 	 else if((ADC_RED < adcTemporal) && (adcTemporal < ADC_BRICK))
 	 	 {
-
+	 		 Red = 255;
+			 Green = 85;
+			 Blue = 0;
+			 PWM_channel_value(Red, Green, Blue);
 	 	 }
 	 	 else if((ADC_BRICK < adcTemporal) && (adcTemporal < ADC_ORANGE))
 	 	 {
+	 		Red = 255;
+			Green = 145;
+			Blue = 0;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	 else if((ADC_ORANGE < adcTemporal) && (adcTemporal < ADC_YELLOW))
 	 	 {
+	 		Red = 255;
+			Green = 247;
+			Blue = 0;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	 else if((ADC_YELLOW < adcTemporal) && (adcTemporal < ADC_YELLOW_LIME))
 	 	 {
+	 		Red = 154;
+			Green = 255;
+			Blue = 0;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	 else if((ADC_YELLOW_LIME < adcTemporal) && (adcTemporal < ADC_GREEN))
 	 	 {
+	 		Red = 0;
+			Green = 255;
+			Blue = 0;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	 else if((ADC_GREEN < adcTemporal) && (adcTemporal < ADC_GREEN_FOREST))
 	 	 {
+	 		Red = 64;
+			Green = 255;
+			Blue = 64;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	else if((ADC_GREEN_FOREST < adcTemporal) && (adcTemporal < ADC_AQUAMARINE))
 	 	 {
+	 		Red = 0;
+			Green = 255;
+			Blue = 128;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	else if((ADC_AQUAMARINE < adcTemporal) && (adcTemporal < ADC_BABY_BLUE))
 	 	 {
+	 		Red = 0;
+			Green = 215;
+			Blue = 125;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
 	 	else if((ADC_BABY_BLUE < adcTemporal) && (adcTemporal < ADC_SKY_BLUE))
 	 	 {
+	 		Red = 0;
+			Green = 215;
+			Blue = 175;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
-	 	 else if((ADC_BABY_BLUE < adcTemporal) && (adcTemporal < ADC_KING_BLUE))
+	 	 else if((ADC_SKY_BLUE < adcTemporal) && (adcTemporal < ADC_KING_BLUE))
 	 	 {
+	 		Red = 0;
+			Green = 175;
+			Blue = 215;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
-	 	 else if((ADC_BABY_BLUE < adcTemporal) && (adcTemporal < ADC_BLUE))
+	 	 else if((ADC_KING_BLUE < adcTemporal) && (adcTemporal < ADC_BLUE))
 	 	 {
+	 		Red = 0;
+			Green = 0;
+			Blue = 255;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
-	 	else if((ADC_BABY_BLUE < adcTemporal) && (adcTemporal < ADC_LAVENDER))
+	 	else if((ADC_BLUE < adcTemporal) && (adcTemporal < ADC_LAVENDER))
 		 {
+	 		Red = 180;
+			Green = 0;
+			Blue = 180;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
-		 else if((ADC_BABY_BLUE < adcTemporalc) && (adcTemporal < ADC_PURPLE))
+		 else if((ADC_LAVENDER < adcTemporal) && (adcTemporal < ADC_PURPLE))
 	 	 {
+			Red = 255;
+			Green = 0;
+			Blue = 255;
+			PWM_channel_value(Red, Green, Blue);
 
 	 	 }
  }
